0a0585164227362fd22fc0b020031ff3d0def0cf
박지연 <163213733+j02on@users.noreply.github.com>
2024-07-23 17:34:24 +0900

    SignUpStartPage퍼블리싱

---
+++ coco-project/src/components/AdminBtn.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import TeacherImg from "../assets/TeacherImg.svg";
+
+function AdminBtn() {
+  return (
+    <StudentWindowAll>
+      <AdminBtnContent>
+        <AdminBtnImg src={TeacherImg} alt="선생님 이미지" />
+        <StudentButton>선생님</StudentButton>
+      </AdminBtnContent>
+    </StudentWindowAll>
+  );
+}
+
+const AdminBtnContent = styled.div`
+  width: 400px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 29px;
+`;
+
+const AdminBtnImg = styled.img``;
+
+const StudentWindowAll = styled.div`
+  width: 400px;
+  height: 400px;
+  border-radius: 10px;
+  border: 0.5px solid #878787;
+  box-shadow: 0px 4px 64px 0px rgba(0, 0, 0, 0.05);
+`;
+
+const StudentButton = styled.button`
+  font-size: 20px;
+  color: #ffffff;
+  font-weight: 400;
+  display: flex;
+  width: 320px;
+  padding: 20px 0px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  border: none;
+  background: #4f4cff;
+  cursor: pointer;
+`;
+
+export default AdminBtn;
---
+++ coco-project/src/components/StudentBtn.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import StudentImg from "../assets/StudentImg.svg";
+
+function StudentBtn() {
+  return (
+    <StudentWindowAll>
+      <StudentBtnContent>
+        <StudentBtnImg src={StudentImg} alt="학생 이미지" />
+        <StudentButton>학생</StudentButton>
+      </StudentBtnContent>
+    </StudentWindowAll>
+  );
+}
+
+const StudentBtnContent = styled.div`
+  width: 400px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 29px;
+`;
+
+const StudentBtnImg = styled.img``;
+
+const StudentWindowAll = styled.div`
+  width: 400px;
+  height: 400px;
+  border-radius: 10px;
+  border: 0.5px solid #878787;
+  box-shadow: 0px 4px 64px 0px rgba(0, 0, 0, 0.05);
+`;
+
+const StudentButton = styled.button`
+  font-size: 20px;
+  color: #ffffff;
+  font-weight: 400;
+  display: flex;
+  width: 320px;
+  padding: 20px 0px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  border: none;
+  background: #4f4cff;
+  cursor: pointer;
+`;
+
+export default StudentBtn;

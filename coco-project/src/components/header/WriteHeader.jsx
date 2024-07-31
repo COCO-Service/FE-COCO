c459228af409de53db6272d8aafefebc95a2ce80
박지연 <163213733+j02on@users.noreply.github.com>
2024-07-31 13:37:29 +0900

    header 수정

--- coco-project/src/components/header/WriteHeader.jsx
+++ coco-project/src/components/header/WriteHeader.jsx
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 import COCOLogo from "../../assets/COCOLogo.svg";
+import { useLocation } from "react-router-dom";
+import { css } from "styled-components";
 
 export const WriteHeader = () => {
+  const { pathname } = useLocation();
+
   return (
     <HeaderAll>
       <Logo src={COCOLogo} />
       <NavAll>
-        <BoardNav>게시판</BoardNav>
-        <WriteNav>글 작성</WriteNav>
-        <MyPageNav>마이페이지</MyPageNav>
+        <BoardNav pathname={pathname}>게시판</BoardNav>
+        <WriteNav pathname={pathname}>글 작성</WriteNav>
+        <MyPageNav pathname={pathname}>마이페이지</MyPageNav>
       </NavAll>
       <LogoutBtn>로그아웃</LogoutBtn>
     </HeaderAll>
@@ -40,21 +44,48 @@ const NavAll = styled.div`
 
 const BoardNav = styled.div`
   font-size: 20px;
-  color: #4f4cff;
-  font-weight: 700;
+  font-weight: 300;
   cursor: pointer;
+
+  ${({ pathname }) =>
+    pathname === "/"
+      ? css`
+          color: #4f4cff;
+          font-weight: 700;
+        `
+      : css`
+          color: #111111;
+        `}
 `;
 
 const WriteNav = styled.div`
   font-size: 20px;
   font-weight: 300;
   cursor: pointer;
+  ${({ pathname }) =>
+    pathname === "/StudentWritingPage"
+      ? css`
+          color: #4f4cff;
+          font-weight: 700;
+        `
+      : css`
+          color: #111111;
+        `}
 `;
 
 const MyPageNav = styled.div`
   font-size: 20px;
   font-weight: 300;
   cursor: pointer;
+  ${({ pathname }) =>
+    pathname === "/"
+      ? css`
+          color: #4f4cff;
+          font-weight: 700;
+        `
+      : css`
+          color: #111111;
+        `}
 `;
 
 const LogoutBtn = styled.div`

206dd4b7699083fa388df31f2e7b7aa97ee49a47
do-hyun123 <shickcoong777@gmail.com>
2024-08-07 21:47:40 +0900

    미적용 코드 커밋

--- coco-project/src/pages/StudentMyPage.jsx
+++ coco-project/src/pages/StudentMyPage.jsx
@@ -1,22 +1,32 @@
 import styled from "styled-components";
+<<<<<<< Updated upstream
 import { Profile } from "../components/adminPage/Profile";
 import { AfterHeader } from "../components/header/AfterHeader";
 import { StudentComplain } from "../components/myPage/StudentComplain";
+=======
+import { AfterHeader } from "../components/header/AfterHeader";
+>>>>>>> Stashed changes
 import { Footer } from "../components/footer/Footer";
 
 export const StudentMyPage = () => {
   return (
     <Container>
+<<<<<<< Updated upstream
       <AfterHeader isMember={true} isStudent={true} />
       <MainContainer>
         <Profile />
         <StudentComplain />
       </MainContainer>
+=======
+      <AfterHeader />
+      <MainContainer></MainContainer>
+>>>>>>> Stashed changes
       <Footer />
     </Container>
   );
 };
 
+<<<<<<< Updated upstream
 const Container = styled.div`
   background-color: white;
 `;
@@ -24,3 +34,8 @@ const Container = styled.div`
 const MainContainer = styled.div`
   display: flex;
 `;
+=======
+const Container = styled.div``;
+
+const MainContainer = styled.div``;
+>>>>>>> Stashed changes
